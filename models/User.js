b36f455ebb009c0ad144502cd0b36535f272fbfe
Barrett Hafner <barretth@gmail.com>
2016-02-26 23:42:47 +0000

    removed password from User model

--- models/User.js
+++ models/User.js
@@ -3,7 +3,6 @@ var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
     username: { type: String, required: true, index: { unique: true } },
-    password: { type: String, required: true },
     email: String,
     starScore: Number,
     list: [{
