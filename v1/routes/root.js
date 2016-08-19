4441d479d537b7cab0c2af11ed42e3c4c454924a
Barrett Hafner <barrett@hafnerindustries.com>
2016-08-19 16:34:44 -0700

    removed v1 files

--- v1/routes/root.js
+++
@@ -1,9 +0,0 @@
-var express     = require("express"),
-    router      = express.Router();
-    
-// Root ("/") route
-router.get("/", function(req, res){
-    res.render("root");
-});
-
-module.exports = router;
